5c87205192347ae3bb0f818c98304516b9d40c1c
Ryzhkova <Tatyana.Va.Ryzhkova@rosbank.ru>
2024-03-11 20:36:46 +0300

    first test

---
+++ app/src/main/java/test/project/SomeClass.java
@@ -0,0 +1,12 @@
+
+package test.project;
+
+class SomeClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
+
